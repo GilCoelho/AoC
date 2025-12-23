04517f50cb57541ea6d200ab0873f7a05c1e67b0
Gil Coelho <gil.sa.coelho@gmail.com>
2025-12-23 12:15:08 +0000

    Start day 1 testing the import of personal input from AoC

---
+++ python/src/test.py
@@ -0,0 +1,8 @@
+import aoc_2025  # noqa: F401
+from aocd.models import Puzzle
+
+# Environment variables are automatically loaded when importing the package
+puzzle = Puzzle(year=2025, day=1)
+
+# Personal input data. Your data will be different.
+print(puzzle.input_data)
