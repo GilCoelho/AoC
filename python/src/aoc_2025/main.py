71c21fed61e2466d36ec3b2be31445434aee6e84
Gil Coelho <gil.sa.coelho@gmail.com>
2025-12-23 16:50:50 +0000

    Resolve part1 of day 1
    Add new module and start unit tests

--- python/src/aoc_2025/main.py
+++ python/src/aoc_2025/main.py
@@ -1,24 +1,79 @@
 import pathlib
 import sys
-import os
 
-# Environment variables are automatically loaded when importing this package
+from aocd.models import Puzzle
+from dotenv import load_dotenv
+
+
+class Dialer:
+    __lower_limit = 0
+    __upper_limit = 99
+
+    def __init__(self):
+        self._position = 50
+
+    def turn(self, turns):
+        self._position += turns
+
+        # Wrap around - dialer is circular
+        while self.__lower_limit > self._position or self._position > self.__upper_limit:
+            if self._position < self.__lower_limit:
+                # is + self.__position because at this point self._position is negative
+                self._position = self.__upper_limit + (self._position + 1)
+            elif self._position > self.__upper_limit:
+                self._position = self.__lower_limit + (self._position - self.__upper_limit - 1)
+
+        print( f"Dialer turned {turns} to position {self._position}" )
+
+    def position(self):
+        return self._position
+
+    def lower_limit(self):
+        return self.__lower_limit
+
+    def upper_limit(self):
+        return self.__upper_limit
 
 def parse(puzzle_input):
     """Parse input."""
-    return [int(line) for line in puzzle_input.split()]
 
-def part1(numbers):
+    puzzle_input = puzzle_input.split() if isinstance(puzzle_input, str) else puzzle_input
+
+    # Return a list of integers, negative for 'L' and positive for 'R' according to first character
+    return [int(line[1:])*-1 if line[0] == 'L' else int(line[1:]) for line in puzzle_input]
+
+def part1(turns):
     """Solve part 1."""
-    for num1 in numbers:
-        for num2 in numbers:
-            if num1 < num2 and num1 + num2 == 2020:
-                return num1 * num2
+    counting_position = 0
+    count = 0
+
+    for action in turns:
+        entrance_safe.turn(action)
+        dialer_pos = entrance_safe.position()
+
+        if counting_position == dialer_pos:
+            count += 1
+
+    print(f"The dialer stopped {count} times on the starting position.")
 
 if __name__ == "__main__":
-    for path in sys.argv[1:]:
-        print(f"\n{path}:")
-        puzzle_input = pathlib.Path(path).read_text().strip()
+    entrance_safe = Dialer()
+    puzzle_input = []
 
-        numbers = parse(puzzle_input)
-        print(part1(numbers))
+    # if command args are given, use them as input files
+    if len(sys.argv) > 1:
+        puzzle_input = pathlib.Path(sys.argv[1]).read_text().strip()
+    else:
+        # Environment variables load
+        project_root = pathlib.Path(__file__).parent.parent.parent
+        env_file = project_root / '.env'
+        load_dotenv(env_file)
+
+        # use personal input data from AoC
+        puzzle_input = Puzzle(year=2025, day=1).input_data
+
+    # parse instructions
+    turns = parse(puzzle_input)
+
+    # solve part 1
+    part1(turns)
---
+++ python/tests/aoc_2025/__init__.py
@@ -0,0 +1,5 @@
+import pathlib
+import sys
+
+# Add the src directory to the Python path
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent / "src"))
